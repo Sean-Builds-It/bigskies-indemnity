89609af38259d3dc42629dd2023b2fa5505c2854
seanbuildsit <seanbuildsthings@gmail.com>
2026-04-16 19:00:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
+var CACHE_NAME = 'bigskies-v1';
+var URLS_TO_CACHE = [
+  '/bigskies-indemnity/',
+  '/bigskies-indemnity/index.html'
+];
 
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      if (cached) return cached;
+      return fetch(e.request).then(function(response) {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, clone);
+        });
+        return response;
+      }).catch(function() {
+        return caches.match('/bigskies-indemnity/index.html');
+      });
+    })
+  );
+});
